e44568cdd6188980e4c301badd8038472c72d6b5
markove <947928019@qq.com>
2015-02-21 11:43:32 +0800

    homework
    homework

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NUll
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+get <- function() x
+setsolve <- function(solve) m <<- solve
+getsolve <- function() m
+list(set = set, get = get, setslove = setsolve, getsolve = getsovle)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+m <- x$getsolve()
+if(!is.null(m)){
+message("getting cached data")
+return(m)
+  }
+data <- x$get()
+m <- solve(data, ¡¤¡¤¡¤)
+x$setsolve(m)
+m
+}
